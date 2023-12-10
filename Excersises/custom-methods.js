d06956893d467fd8a95e48bdbc5f0563b3007ce4
varunnaik1121 <varunnaik1121@gmail.com>
2023-12-11 00:04:23 +0530

    feat:final update on custom function of arrays

--- Excersises/custom-methods.js
+++ Excersises/custom-methods.js
@@ -12,7 +12,7 @@ const arr = [1, 2, 3, 4];
 const modifiedArr = customMap(arr, (item, i) => {
   return item * i;
 });
-console.log(modifiedArr);
+// console.log(modifiedArr);
 
 //method 2 using the prototypes
 
@@ -28,7 +28,7 @@ Array.prototype.customMap = function (callback) {
 const modifiedProArr = proArr.customMap((item) => {
   return item * 10;
 });
-console.log({ modifiedProArr });
+// console.log({ modifiedProArr });
 
 //ways to created custom Array methods (Custom forEach)
 
@@ -41,7 +41,7 @@ const forEachArr = [1, 2, 3, 4, 5];
 const modified = forEachArr.customforEach((item) => {
   return item * 1;
 });
-console.log({ modified });
+// console.log({ modified });
 
 //ways to created custom Array methods (Custom Reduce)
 
@@ -49,18 +49,56 @@ const reduceArr = [1, 2, 3, 4, 5];
 const reducedValue = reduceArr.reduce((acc, currValue) => {
   return acc + currValue;
 }, 10);
-console.log(reducedValue); //prints 25
+// console.log(reducedValue); //prints 25
 
 Array.prototype.customReduce = function (callback, initialValue) {
-  let accumulator = initialValue;
+  var accumulator = initialValue;
   for (let i = 0; i < this.length; i++) {
-    accumulator=accumulator?callback(accumulator,this[i],i,this):this[i];
+    accumulator = accumulator
+      ? callback(accumulator, this[i], i, this)
+      : this[i];
   }
   return accumulator;
 };
 
-console.log(
-  reduceArr.customReduce((acc, currVal) => {
-    return acc + currVal;
-  }, 10)
-);
+// console.log(
+//   reduceArr.customReduce((acc, currVal) => {
+//     return acc + currVal;
+//   }, 0)
+// );
+
+//custom function for filter
+//method - 1  (using the normal function)
+
+function customFilter(array, callback) {
+  let temp = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array) == true) {
+      temp.push(array[i]);
+    }
+  }
+  return temp;
+}
+
+const filterArr = [1, 2, 3, 4, 5];
+const filteredValues = customFilter(filterArr, (item) => {
+  return item >= 3;
+});
+
+console.log({ filteredValues });
+
+//method 2 (By using the built in prototype of array)
+Array.prototype.customFilter = function (callback) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      temp.push(this[i]);
+    }
+    continue;
+  }
+  return temp;
+};
+
+// console.log(
+//   ['varun', 'kiran', 'nishal'].customFilter((item) => item.includes('r'))
+// );
