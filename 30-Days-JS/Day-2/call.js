ad8577dbefaa668b8f121f8ae4c6bda12696427b
varunnaik1121 <varunnaik1121@gmail.com>
2023-12-14 11:06:32 +0530

    feat:added call definittion

---
+++ 30-Days-JS/Day-2/call.js
@@ -0,0 +1,20 @@
+let printName = function (branch, regNo) {
+  console.log(
+    `My name is${
+      (this.firstName, this.lastName)
+    } and I am persuing ${branch} and my reg no is ${regNo}`
+  );
+};
+
+let user1 = {
+  firstName: 'varun',
+  lastName: 'naik',
+};
+
+let user2 = {
+  firstName: 'kiran',
+  lastName: 'kumar',
+};
+
+printName.call(user2, 'BCA', '1DS22MC112');
+//call method is used to call the function of objects with substituting another objects
